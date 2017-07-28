a1486090d6d9c59fae9cfb8a3dd0795a0a388c29
Dejan Saric <dsaric@mail.usf.edu>
2017-07-28 15:05:22 -0400

    Nearly done

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mustacheExpress = require('mustache-express')
 const expressValidator = require('express-validator')
 const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
+
+let underscores = []
+let guesses = []
+let secretword = []
+let count = 8
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,8 +19,42 @@ app.engine('mst', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mst')
 
+let randomWord = function() {
+  let i = Math.floor(Math.random() * words.length)
+  return words[i]
+}
+
+let newword = randomWord()
+console.log(newword)
+
+for (let j = 0; j < newword.length; j++) {
+  secretword.push(newword[j])
+}
+
+underscores = secretword.map(x => {
+  return (x = '_')
+})
+
+console.log(secretword)
+console.log(underscores)
+
 app.get('/', (req, res) => {
-  res.send('NIBBA WE MADE IT')
+  res.render('home', { underscores: underscores, guesses: guesses, count: count })
+})
+
+app.post('/', (req, res) => {
+  if (secretword.includes(req.body.guess)) {
+    for (let q = 0; q < secretword.length; q++) {
+      if (secretword[q] === req.body.guess) {
+        underscores.splice(q, 1, req.body.guess)
+      }
+    }
+  } else {
+    count -= 1
+    console.log(count)
+  }
+  guesses.push(req.body.guess)
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
